a98cd6c2afed778bb327d09fca4c4fa07cf5d371
Nana Muthuswamy <narayanan_eie@yahoo.com>
2018-12-22 20:03:05 -0800

    Project Setup

---
+++ TrackIt WatchKit Extension/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  TrackIt WatchKit Extension
+//
+//  Created by Nana on 12/22/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ TrackIt WatchKit Extension/WeightRow.swift
@@ -0,0 +1,13 @@
+//
+//  WeightRow.swift
+//  TrackIt WatchKit Extension
+//
+//  Created by Nana on 12/22/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import WatchKit
+
+class WeightRow: NSObject {
+
+}
